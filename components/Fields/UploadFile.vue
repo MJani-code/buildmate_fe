928a74517fced8e5e88b0f674cb3084c71e9ab60
air <air@Apple-MacBook-Air.local>
2023-05-27 19:35:13 +0200

    input and select fields have been align next to each other

--- components/Fields/UploadFile.vue
+++ components/Fields/UploadFile.vue
@@ -2,40 +2,47 @@
     <v-dialog :value="uploadDialog" @click:outside="closeDialog" class="dialog-width" width="auto">
         <v-card class="p-20">
             <v-form ref="form" @submit.prevent="onSubmit">
-                <v-text-field v-model="item.name" label="Dokumentum címe" class="input-group" color="#359756" required
-                    :rules="titleRule">
-                </v-text-field>
-                <v-select color="#359756" :items="doctypes" item-text="typeHun" item-value="id"
-                    label="Típus">
-                </v-select>
+                <v-row>
+                    <v-col class="pa-2 col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6">
+                        <v-text-field v-model="item.name" label="Dokumentum címe" class="input-group" color="#359756"
+                            required :rules="titleRule">
+                        </v-text-field>
+                    </v-col>
+                    <v-col class="pa-2 col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6">
+                        <v-select color="#359756" :items="doctypes" item-text="typeHun" item-value="id" label="Típus">
+                        </v-select>
+                    </v-col>
+                </v-row>
                 <!-- TODO: dokumentum típusait itt fel kell venni egy legördülőbe -->
-                <div class="block items-center justify-center text-center">
-                    <div class="p-50 bg-gray-100 border border-gray-300" @dragover="dragover" @dragleave="dragleave"
-                        @drop="drop">
-                        <input type="file" model="filelist" name="fields[assetsFieldHandle][]" id="assetsFieldHandle"
-                            class="w-px h-px opacity-0 overflow-hidden absolute" @change="onChange" ref="file"
-                            accept=".pdf,.jpg,.jpeg,.png" :rules="fileRule" required />
-                        <div>
-                            Húzd ide a feltölteni kívánt dokumentumot vagy kattints
-                            <label for="assetsFieldHandle" class="block cursor-pointer">
-                                <span class="underline"><a>ide</a></span>
-                            </label>
-                        </div>
-                        <v-row class="mt-4" v-if="this.filelist.length">
-                            <div v-for="file in filelist">
-                                <v-icon size="30" color="#359756">
-                                    mdi-file
-                                </v-icon>
-                                <v-card-text>
-                                    {{ file.name }}
-                                    <v-icon class="ml-2" size="20" @click="remove(filelist.indexOf(file))">
-                                        mdi-close
-                                    </v-icon>
-                                </v-card-text>
+                <v-row>
+                    <div class="block items-center justify-center text-center">
+                        <div class="p-50 bg-gray-100 border border-gray-300" @dragover="dragover" @dragleave="dragleave"
+                            @drop="drop">
+                            <input type="file" model="filelist" name="fields[assetsFieldHandle][]" id="assetsFieldHandle"
+                                class="w-px h-px opacity-0 overflow-hidden absolute" @change="onChange" ref="file"
+                                accept=".pdf,.jpg,.jpeg,.png" :rules="fileRule" required />
+                            <div>
+                                Húzd ide a feltölteni kívánt dokumentumot vagy kattints
+                                <label for="assetsFieldHandle" class="block cursor-pointer">
+                                    <span class="underline"><a>ide</a></span>
+                                </label>
                             </div>
-                        </v-row>
+                            <v-row class="mt-4" v-if="this.filelist.length">
+                                <div v-for="file in filelist">
+                                    <v-icon size="30" color="#359756">
+                                        mdi-file
+                                    </v-icon>
+                                    <v-card-text>
+                                        {{ file.name }}
+                                        <v-icon class="ml-2" size="20" @click="remove(filelist.indexOf(file))">
+                                            mdi-close
+                                        </v-icon>
+                                    </v-card-text>
+                                </div>
+                            </v-row>
+                        </div>
                     </div>
-                </div>
+                </v-row>
                 <v-card-actions class="mt-10">
                     <v-btn @click="closeDialog()">Mégsem</v-btn>
                     <v-btn type="submit" color="#359756">Mentés</v-btn>
@@ -58,8 +65,9 @@ export default {
     data() {
         return {
             doctypes: [
-                {id:1, typeEng: 'invoice', typeHun: 'számla'},
-                {id:2, typeEng: 'document', typeHun: 'dokumentum'},
+                { id: 1, typeEng: 'invoice', typeHun: 'számla' },
+                { id: 2, typeEng: 'document', typeHun: 'dokumentum' },
+                { id: 3, typeEng: 'other', typeHun: 'egyéb' },
             ],
             delimiters: ['${', '}'], // Avoid Twig conflicts
             filelist: [], // Store our uploaded files
