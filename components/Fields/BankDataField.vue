e87e0ef8441a31db857e308df61a88e7deed6480
Jani <martonjanos1990@gmail.com>
2023-05-30 22:54:28 +0200

    amount is also added with validation rules

--- components/Fields/BankDataField.vue
+++ components/Fields/BankDataField.vue
@@ -11,13 +11,14 @@
             </v-text-field>
         </v-col>
         <v-col class="pa-2 col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6">
-            <v-text-field v-model="bankData.accountNumber"
-                :rules="[letterRule, cardNumberRule]" @input="formatCardNumber" label="Bankszámlaszám" class="input-group"
-                color="#359756" required>
+            <v-text-field v-model="bankData.accountNumber" :rules="[letterRule, cardNumberRule]" @input="formatCardNumber"
+                label="Bankszámlaszám" class="input-group" color="#359756" required>
             </v-text-field>
         </v-col>
         <v-col class="pa-2 col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6">
-
+            <v-text-field ref="amount" v-model="bankData.amount" :rules="[letterRule, formatAmountRule]"
+                @input="formatAmountValue" label="Utalandó összeg" class="input-group" color="#359756" required>
+            </v-text-field>
         </v-col>
     </v-row>
 </template>
@@ -34,15 +35,20 @@ export default {
                 firstName: '',
                 lastName: '',
                 accountNumber: null,
+                amount: ''
             },
             nameRule: [v => !!v || 'Kötelező kitölteni'],
             cardNumberRule: (v) => /^\d{8}-\d{8}-\d{8}$/.test(v) || 'Érvénytelen bankszámlaszám',
             letterRule: (v) => {
                 const hasLetter = /[a-zA-Z]/.test(v);
-                if (hasLetter) {
-                    this.bankData.accountNumber = null; // Bevitt érték törlése
+                if (hasLetter && this.bankData.accountNumber !== null) {
+                    this.bankData.accountNumber.slice(0, -1);
+                    v.slice(0, -1);
                 }
                 return !hasLetter || 'Csak számokat lehet beírni';
+            },
+            formatAmountRule: (v) => {
+                return !isNaN(parseFloat(v));
             },
         };
     },
@@ -62,6 +68,23 @@ export default {
                 }
             }
         },
+        formatAmountValue() {
+            const formattedValue = this.formatAmount(this.bankData.amount);
+            this.bankData.amount = formattedValue;
+        },
+        formatAmount(value) {
+            if (value === null || value === "") {
+                return value;
+            }
+            const numericValue = parseFloat(value.replace(/[^0-9.-]+/g, ""));
+            if (isNaN(numericValue)) { // Ellenőrizd, hogy a numericValue NaN-e
+                return ""; // Vagy a kívánt alapértelmezett értéket adhatod vissza
+            }
+            const formattedValue = new Intl.NumberFormat("hu-HU").format(numericValue);
+            return formattedValue;
+        },
+
+
     }
 }
 
