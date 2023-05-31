778b9ff04e010c1ed84791905cf33c3630398e6b
Jani <martonjanos1990@gmail.com>
2023-05-31 18:01:00 +0200

    vuetify css is added

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,7 +32,9 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: [
+    'vuetify/dist/vuetify.css',
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
