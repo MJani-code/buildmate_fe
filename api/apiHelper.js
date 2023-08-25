b3fca5d2fd68a19a2686c6dc02078a36f1461a2e
Jani <martonjanos1990@gmail.com>
2023-08-25 15:01:37 +0200

    .

---
+++ api/apiHelper.js
@@ -0,0 +1,42 @@
+// i need an axios base config and functions like APIPOST APIGET APIPUT APIDELETE
+
+import axios from 'axios';
+
+const config = {
+  apiUrl: {
+
+    // Itt további végpontokat adhatsz meg
+  },
+};
+
+const API = axios.create({
+    baseURL: process.env.API_URL ?? 'http://',
+    timeout: 3000
+    // headers: {'X-Custom-Header': 'foobar'}
+});
+
+export const APIPOST = async (endpoint, data) => {
+    const url = config.apiUrl[endpoint];
+    return await API.post(url, data);
+}
+
+export const APIGET = async (endpoint) => {
+  const url = config.apiUrl[endpoint];
+  return await API.get(url);
+}
+
+export const APIPUT = async (url, data) => {
+  return await API.put(url, data);
+}
+
+export const APIDELETE = async (url) => {
+  return await API.delete(url);
+}
+
+export const APIUPLOAD = async (url, data) => {
+  return await API.post(url, data, {
+    headers: {
+      'Content-Type': 'multipart/form-data'
+    }
+  });
+}
--- mixins/aos.js
+++ mixins/aos.js
@@ -1,9 +1,8 @@
-// mixins/aos.js
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 
 export default {
   mounted() {
     AOS.init({ disable: 'phone' })
-  },
+  }
 }
