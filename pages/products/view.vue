972b0739fb85defcdd11c7396fe0e37417265c6a
Jani <martonjanos1990@gmail.com>
2023-09-15 00:01:46 +0200

    .

--- pages/products/view.vue
+++ pages/products/view.vue
@@ -1,51 +1,53 @@
 <template>
+  <div class="w-full">
+    <section class="w-full pb-24"></section>
 
-    <div class="w-full">
-      <section class="w-full pb-24"></section>
-      <section
-        class="max-w-screen-xl mx-2 sm:mx-auto px-4 sm:px-6 lg:px-0 py-6 pb-20 sm:py-8 rounded-[2.25rem] sm:rounded-xl bg-white shadow-lg sm:shadow-md transform lg:-translate-y-12"
-        :style="{ 'display': 'flex' }"
-      >
-      <v-app class="m-4">
-        <div :style="{ 'max-width': '700px' }">
-          <v-carousel hide-delimiters>
-            <v-carousel-item
-              v-for="(item, i) in items"
-              :key="i"
-              :src="item.src"
-            ></v-carousel-item>
-          </v-carousel>
+    <section
+      class="max-w-screen-xl mx-2 sm:mx-auto px-4 sm:px-6 lg:px-0 py-6 pb-20 sm:py-8 rounded-[2.25rem] sm:rounded-xl bg-white shadow-lg sm:shadow-md transform lg:-translate-y-12"
+      :style="{ display: 'flex' }"
+    >
+      <div :style="{ 'width': '700px' }">
+        <v-app class="m-4">
+          <template>
+            <v-carousel hide-delimiters>
+              <v-carousel-item
+                v-for="(item, i) in items"
+                :key="i"
+                :src="item.src"
+                eager
+              ></v-carousel-item>
+            </v-carousel>
+          </template>
+        </v-app>
+      </div>
+      <div class="p-8">
+        <a href="#">
+          <h5 class="text-xl font-semibold tracking-tight">Cím</h5>
+        </a>
+        <div>
+          <p>Vállalkozás neve</p>
         </div>
-      </v-app>
-        <div class="p-8">
-          <a href="#">
-            <h5 class="text-xl font-semibold tracking-tight">Cím</h5>
-          </a>
-          <div>
-            <p>Vállalkozás neve</p>
-          </div>
-          <div>
-            <p>Beváltóhely</p>
-          </div>
-          <div>
-            <p>Kategória</p>
-          </div>
-          <div>
-            <p>részletek</p>
-          </div>
-          <div>
-            <p>tagek</p>
-          </div>
-          <div>
-            <p>mennyiség</p>
-          </div>
-          <div class="flex items-center justify-between">
-            <span class="text-3xl font-bold">$599</span>
-          </div>
+        <div>
+          <p>Beváltóhely</p>
         </div>
-      </section>
-    </div>
-
+        <div>
+          <p>Kategória</p>
+        </div>
+        <div>
+          <p>részletek</p>
+        </div>
+        <div>
+          <p>tagek</p>
+        </div>
+        <div>
+          <p>mennyiség</p>
+        </div>
+        <div class="flex items-center justify-between">
+          <span class="text-3xl font-bold">$599</span>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
@@ -55,16 +57,16 @@ export default {
       product: [],
       items: [
         {
-          src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+          src: require("~/assets/img/7446783_3661698.png"),
         },
         {
-          src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
+          src: require("~/assets/img/11235588_10814.jpg"),
         },
         {
-          src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
+          src: require("~/assets/img/eco-system-5019188-4196246.png"),
         },
         {
-          src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
+          src: require("~/assets/img/save-planet-8429926-6723330.png"),
         },
       ],
     };
